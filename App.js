9850a89dadf70c8bd7ad84ad1c251c1c511fbcb6
Zeq <Ezequiellopez.dev@gmail.com>
2023-12-29 03:37:22 -0300

    FlatList

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, Image, View, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [pokemons, setPokemons] = useState(null)
+
+	useEffect(() => {
+		fetch('https://pokeapi.co/api/v2/pokemon')
+			.then(response => response.json())
+			.then(result => setPokemons(result.results))
+	}, [])
+
+	return (
+		<FlatList
+			data={pokemons}
+			renderItem={({ item: pokemon }) => (
+				<View style={styles.card}>
+					<Image
+						style={styles.img}
+						source={{
+							uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+								pokemon.url.split('/').reverse()[1]
+							}.png`,
+						}}
+					/>
+					<Text style={styles.text}>{pokemon.name}</Text>
+				</View>
+			)}
+			ItemSeparatorComponent={<Text> </Text>}
+			style={styles.container}
+		>
+			<StatusBar style='auto' />
+		</FlatList>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		backgroundColor: '#000',
+	},
+	card: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		color: '#fff',
+		textTransform: 'uppercase',
+	},
+	img: {
+		width: 99,
+		height: 99,
+	},
+})
